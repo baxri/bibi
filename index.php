20485b35370b60cf2b839bf44f42439e913f1122
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-02-03 03:38:37 +0400

    --

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 
 <script src="bower_components/showdown/dist/showdown.js"></script>
 
-<script src="text/javascript">
+<script type="text/javascript">
 var converter = new showdown.Converter(),
     text      = '#hello, markdown!',
     html      = converter.makeHtml(text);
+
 </script>
