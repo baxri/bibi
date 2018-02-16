b7535730a9fa89b885840f62421276166f9fc4ae
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-02-16 13:43:25 +0400

    --

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>georgeBIBIlashvili</title>
+    <title>www.BIBI.ge</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="bower_components/highlightjs/styles/tomorrow-night.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
@@ -11,14 +11,17 @@
     <script src="bower_components/showdown/dist/showdown.js"></script>    
     <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/highlightjs/highlight.pack.js"></script>   
-        `
+        
     <script type="text/javascript"> 
         $(document).ready(function(){
-            $.get('README.md', function(markdown) {           
+            $.get('README.md', function(markdown) {  
+                
+                console.log("OK");
+
                 converter = new showdown.Converter();   
                 $("#showdown").html(converter.makeHtml(markdown));
                 
-                hljs.initHighlightingOnLoad();                
+                // hljs.initHighlightingOnLoad();                
                 setTimeout(function(){
                     $("#showdown").fadeIn('slow');
                 }, 500);
