eb70f798a5c619a4ea2d78a2d008a735ed186089
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-02-24 22:26:28 +0400

    --

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
 var gulp = require('gulp');
 var less = require('gulp-less'); 
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload; 
- 
-/* Task to compile less */
-gulp.task('compile-less', function() {  
-  gulp.src('./css/main.less')
-    .pipe(less())
-    .pipe(gulp.dest('./css/'));
-}); 
-/* Task to watch less changes */
 
-gulp.task('watch-less', function() {  
-  gulp.watch('./css/*.less' , ['compile-less']);
+/* Task to compile less */
+gulp.task('compile-less', function() {
+    gulp.src('./css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./css/'));
+});
+
+/* Task to watch less changes */
+gulp.task('watch', function() {
+    gulp.watch('./css/*.less' , ['compile-less']);
 });
  
  
-/* Task when running `gulp` from terminal  'watch-less',*/
-gulp.task('default', [ 'compile-less', 'watch-less']);
+/* Task when running `gulp` */
+gulp.task('default', [ 'compile-less']);
--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>www.BIBI.ge</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="bower_components/jquery/dist/jquery.js"></script>
+
 <link rel="stylesheet" type="text/css" media="screen" href="css/main.css"/>
+
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="js/main.js"></script>
+
 </head>
 <body>
 <div id="showdown"></div>
