696bbcbc1cf1d8317ff7a5fb9a60591eba68233c
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-03-01 23:42:37 +0400

    --

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>www.BIBI.ge</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'>
 <link rel="stylesheet" type="text/css" media="screen" href="public/css/main.css"/>
 <script src="public/js/app.js"></script>
 </head>
 <body>
-<div id="showdown">aklad</div>
+<div class="text">&lt;?php ?&gt;</div>
+
+<div class="footer">
+    <h1>George Bibilashvili</h1>
+    <h3>Web Developer</h3>
+    <p>giorgi.bibilashvili89@gmail.com</p>
+    <p><a href="https://github.com/baxri">https://github.com/baxri</a></p>
+</div>
+
 </body>
 </html>
