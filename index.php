9743b81b4f3360039b4951257e292eb06d88861f
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-03-02 00:00:27 +0400

    --

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8"/>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>www.BIBI.ge</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'>
-<link rel="stylesheet" type="text/css" media="screen" href="public/css/main.css"/>
-<script src="public/js/app.js"></script>
+    <title>www.BIBI.ge</title>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" media="screen" href="public/css/main.css"/>
+    <script src="public/js/app.js"></script>
 </head>
 <body>
 <div class="text">&lt;?php ?&gt;</div>
-
 <div class="footer">
     <h1>George Bibilashvili</h1>
     <h3>Web Developer</h3>
     <p>giorgi.bibilashvili89@gmail.com</p>
     <p><a href="https://github.com/baxri">https://github.com/baxri</a></p>
+    <p><a href="https://www.linkedin.com/in/giorgi-bibilashvili-1b620761/">Linkdin</a></p>
 </div>
-
 </body>
 </html>
